120f6e3f064272b163cf80dd5811118448da05ec
janakutty9751 <janarthanan.raja@prodian.co.in>
2024-11-18 12:11:08 +0530

    nothing

--- src/app/components/project/project.component.ts
+++ src/app/components/project/project.component.ts
@@ -11,7 +11,7 @@ export class ProjectComponent {
     {
       hind:'SPORTS BASED',
       name: 'SPORTS RULER WEB',
-      imageUrl: '/assets/images/sportsruler.png',
+      imageUrl: './assets/images/sportsruler.png',
       website:'http://www.sportsruler.com/SR/home',
       description: 'SportsRuler is an endeavor to get all players of the Sports Eco system under one platform. It can help record scores, compute statistics, provide analytics just like how professionals would get access to data and information, every single passionate person can too.',
       visible: true
@@ -19,7 +19,7 @@ export class ProjectComponent {
     {
       hind:'SPORTS BASED',
       name: 'SPORTS RULER SCORING APP',
-      imageUrl: '/assets/images/sportsrulerApp.png',
+      imageUrl: './assets/images/sportsrulerApp.png',
       website:'http://www.sportsruler.com/SR/home',
       description: 'SportsRuler is an app designed for updating live cricket scores. It allows users to record match scores, compute statistics, and provide real-time analytics. Whether you’re a professional or a passionate cricket fan, SportsRuler helps you update the score and keep track of the game with ease.',
       visible: true
@@ -27,7 +27,7 @@ export class ProjectComponent {
     {
       hind:'COMPANY SITE',
       name: 'PRODIAN INFO TECH',
-      imageUrl: '/assets/images/prodian.png',
+      imageUrl: './assets/images/prodian.png',
       website:'http://www.sportsruler.com/SR/home',
       description: "It's a company website for my current workplace, developed using Angular, TypeScript, and Spring Boot. In this site, I have incorporated common technologies such as Angular Material, Bootstrap, and shared services for efficient and reusable components.",
       visible: true
@@ -36,7 +36,7 @@ export class ProjectComponent {
       hind:'BANK BASED (UI LEVEL)',
       name: 'ANTI BRIBERY SYSTEM ',
       website:'http://www.sportsruler.com/SR/home',
-      imageUrl: '/assets/images/ABS.png',
+      imageUrl: './assets/images/ABS.png',
       description: 'Morgan has collected ants since they were six years old and now has many dozen ants but none in their pants.',
       visible: true
     },
@@ -52,7 +52,7 @@ export class ProjectComponent {
       hind:'BANK BASED (UI LEVEL)',
       name: 'RENEWAL RETURNTION CALLING SYSTEM',
       website:'http://www.sportsruler.com/SR/home',
-      imageUrl: '/assets/images/RRCS.png',
+      imageUrl: './assets/images/RRCS.png',
       description: 'Adrian has collected flies since they were six years old and now has many dozen flies but none in their pants.',
       visible: true
     },
